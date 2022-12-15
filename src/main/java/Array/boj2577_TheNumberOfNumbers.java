8f65dfb94526f2ea4deb0caa880fec31278ec8a7
lyg509 <dydrjs252@naver.com>
2022-12-15 12:07:09 +0900

    [Array]boj2577_TheNumberOfNumbers

---
+++ src/main/java/Array/boj2577_TheNumberOfNumbers.java
@@ -0,0 +1,28 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj2577_TheNumberOfNumbers {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[10];
+
+        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+
+        String str = String.valueOf(val);
+
+        for (int i = 0; i < str.length(); i++) {
+            arr[(str.charAt(i) - 48)]++;
+        }
+
+        for (int v : arr) {
+            System.out.println(v);
+        }
+
+    }
+}
